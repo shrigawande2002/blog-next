2796645acd6f889e28e25b93e7274acaabd42946
Shrihari <gawandesj@rknec.edu>
2025-05-28 05:18:10 +0530

    added modal and some changes

--- Components/AuthModal.jsx
+++ Components/AuthModal.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+
+import React, { act } from 'react'
 import { Modal } from 'antd'
 import PropTypes from 'prop-types'
 import { MdEmail } from "react-icons/md";
-import { RiLockPasswordLine } from "react-icons/ri";
+import { FaLock } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { loginAction, registerAction } from "@/actions/login";
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
+import { useActionState } from 'react';
+
 const AuthModal = ({ open, setOpen }) => {
     
     const [isLogin, setIsLogin] = React.useState(true);
+    const [state, action, isPending] = useActionState(undefined, loginAction)
     
     return (
         <Modal
             open={open}
             onCancel={() => setOpen(false)}
             footer={null}
-            width={{ xs: "90%", sm: 500, md: 600, lg: 700, xl: 800, xxl: 500 }}
+            width={{ xs: "90%", sm: 500, md: 500, lg: 500, xl: 500, xxl: 500 }}
             centered
         >
 
@@ -20,37 +28,38 @@ const AuthModal = ({ open, setOpen }) => {
                 <h1 className='font-geist text-xl font-semibold text-darkprimary  flex  justify-center '> {isLogin ? "Login" : "Register"} </h1>
 
 
-                <div className='flex flex-col gap-4 mt-10 justify-center items-center'>
+                <form action={action} className='flex flex-col gap-4 mt-10 justify-center items-center'>
 
                     <div className='flex flex-col gap-4 justify-center'>
 
-                        <label htmlFor="name" className='text-darkprimary font-semibold font-geist text-sm '>  Name </label>
+                        {!isLogin && (<> <label htmlFor="name" className='text-darkprimary font-semibold font-geist text-sm '>  Name </label>
                         <p className='flex items-center gap-2 mt-4'>
-                            <MdEmail size={22} className='' />
-                            <input type="text" placeholder='Name' className='outline-none border-b w-96 ' />
-                        </p>
+                                <FaUserCircle size={22} className='' />
+                                <input type="text" placeholder='Name' autoComplete='off' className='outline-none border-b w-96 rounded-md  ' />
+                            </p></>)}
                         <label htmlFor="email" className='text-darkprimary font-semibold font-geist text-sm '>  Email </label>
                         <p className='flex items-center gap-2 mt-4'>
                             <MdEmail size={22} className='' />
-                            <input type="email" placeholder='E-mail' className='outline-none border-b w-96 ' />
+                            <input type="email" placeholder='E-mail' autoComplete='off' className='outline-none border-b w-96 ' />
                         </p>
 
                         <label htmlFor="password" className='text-darkprimary font-semibold font-geist text-sm mt-4'>  Password </label>
                         <p className='flex items-center gap-2 mt-4'>
-                            <RiLockPasswordLine size={22} className='' />
-                            <input type="password" placeholder='Password' className='outline-none border-b w-96 ' />
+                            <FaLock size={22} className='' />
+                            <input type="password" placeholder='Password' autoComplete='off' className='outline-none border-b w-96 ' />
                         </p>
-                        <label htmlFor="conformpassword" className='text-darkprimary font-semibold font-geist text-sm mt-4'> Conform Password </label>
+
+                        {!isLogin && (<>  <label htmlFor="conformpassword" className='text-darkprimary font-semibold font-geist text-sm mt-4'> Conform Password </label>
                         <p className='flex items-center gap-2 mt-4'>
-                            <RiLockPasswordLine size={22} className='' />
+                                <FaLock size={22} className='' />
                             <input type="password" placeholder='conform Password' className='outline-none border-b w-96 ' />
-                        </p>
+                            </p></>)}
 
 
                     </div>
-                    <button className='bg-yellow border-none font-geist text-sm font-semibold  p-2 rounded-md  px-4'> Login </button>
-                    <p className='text-sm text-gray-500'>Don't have an account? <button onClick={() => setOpen(false)} className='text-yellow font-semibold'> Register </button> </p>
-                </div>
+                    <button className='bg-yellow border-none font-geist text-sm font-semibold  p-2 rounded-md  px-4'> {isLogin ? "Login" : "Register"} </button>
+                    <p className='text-sm text-gray-500'>Don't have an account? <button onClick={() => setIsLogin(!isLogin)} className='text-yellow font-semibold'> {isLogin ? "Register" : "Login"} </button> </p>
+                </form>
                  
             </div>
 
